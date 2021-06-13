53611285dd04e1bda874de6a80eae57fa0116d9f
Ami-Kimura <82437518+Ami-Kimura@users.noreply.github.com>
2021-06-13 14:13:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@
 // body の背景色を変える
 function MyFunction01() {
   var obj = document.body;
-  obj.style.backgroundColor = 'red';
+  obj.style.backgroundColor = 'tomato';
 }
 
 
 // ID要素の背景色を変える
 function MyFunction02(){
   var obj = document.getElementById('ID01');
-  obj.style.backgroundColor = 'yellow';
+  obj.style.backgroundColor = 'gold';
 }
 
 
@@ -21,9 +21,9 @@ function MyFunction03(){
     // Class01の把握（情報が line[0], line[1] ,line[2]・・に一括で入る）
     var lines = document.getElementsByClassName('Class01');
 
-    //  i 番目の pタグ部分の背景を green に
+    //  i 番目の pタグ部分の背景を palegreen に
     for( var i=0; i<lines.length; i++ ){
-          lines[i].style.backgroundColor = 'green';
+          lines[i].style.backgroundColor = 'palegreen';
     }
 }
 
@@ -36,7 +36,7 @@ function MyFunction04() {
   // pタグの存在する数だけ処理をくりかえす
   for( var i=0; i<obj.length; i++ ) {
        //  i 番目の pタグ部分の背景を blue に
-      obj[ i ].style.backgroundColor = "blue";
+      obj[ i ].style.backgroundColor = "lightskyblue";
   }
 }
 
